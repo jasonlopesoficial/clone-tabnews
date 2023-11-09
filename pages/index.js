6dad7fdc2cb607908b2ef426f61a6625fe220d0c
Jason Lopes <96127214+jasonlopesoficial@users.noreply.github.com>
2023-11-09 09:49:31 +0000

    Modificação da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Este projeto é um clone do Tabnews e está sendo construído com React e Next.js</h1> 
+    return <h1>Este projeto é um clone do Tabnews e está sendo construído com React e Next.js com Versel</h1> 
 
 }
 
